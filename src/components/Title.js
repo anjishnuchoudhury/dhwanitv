5c848ae15595fcd05f8963dba0c5e84d2b91ac10
Thisisashutosh <thisisashutoshdubey@gmail.com>
2024-01-21 16:35:08 +0530

    edited header and title

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React from 'react'
 
-export default class Title extends React.Component{
-  render(){
-    return(
-      <div className="backdrop-image">       
-        <div className="text-white text-center title-padding container">
-            <label className="title-title mb-4">Tv Dhwani</label>
-            <p className="title-sub-title font-weight-light mb-5">An unforgettable experience</p>
-            {/* <a href="/events" className="btn btn-tv-home col-xl-3 col-md-4 col-8">Upcoming events</a> */}
-            {/* <a href="/tv" className="btn btn-tv-home col-xl-3 col-md-4 col-8">Watch Now</a> */}
+export default class Title extends React.Component {
+  render () {
+    return (
+      <div className='backdrop-image'>
+        <div className='text-white text-left title-padding container-fluid'>
+          <label className='title-title mb-4'>Tv Dhwani</label>
+          <figure class='text-end h4'>
+            <blockquote class='blockquote'>
+              <p className='h3'>When words leave off, music begins.</p>
+            </blockquote>
+            <div className=''>
+              <figcaption class='blockquote-footer text-white text-xl'>
+                Heinrich Heine
+              </figcaption>
+            </div>
+          </figure>
+          <a
+            href='/events'
+            className='btn btn-tv-home mt-5 col-xl-2 text-white col-md-4 col-6'
+          >
+            Upcoming events
+          </a>
+          {/* <a href="/tv" className="btn btn-tv-home col-xl-3 col-md-4 col-8">Watch Now</a> */}
         </div>
       </div>
     )
