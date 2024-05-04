362a4bd877d1a37ad920903fe5c9697072f02be3
Thisisashutosh <thisisashutoshdubey@gmail.com>
2024-05-04 19:16:17 +0530

    Made homepage but removed navbar and problem in icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,96 +1,167 @@
-import React from 'react'
+import React from "react";
 
-export default class Footer extends React.Component {
-  render () {
-    return (
-      <>
+const Footer = () => {
+  return (
+    <div>
+      <div className="mt-5 bg-primary w-full">
         <footer
-          className='footer-bg text-center text-white'
-          id={this.props.footer}
+          className="text-start text-lg-start text-white"
+          style={{ backgroundColor: "#1c2331" }}
         >
-          <div className='d-flex flex-column flex-md-row footerdimensions'>
-            <div className='contact-info'>
-              <h1
-                className='font-weight-bold text-sm-10 line-height-2 appear-animation'
-                data-appear-animation='fadeInUpShorter'
-                data-appear-animation-delay='200'
-              >
-                Contact Info
-              </h1>
-              <p
-                className='lead px-0 mb-0 appear-animation text-sm-5 textSize text-style'
-                data-appear-animation='fadeInUpShorter'
-                data-appear-animation-delay='300'
-              >
-                Email :{' '}
-                <strong className='text-dark'>info@dhwaniacademy.net</strong>
-              </p>
-              <p
-                className='lead px-0 mb-0 appear-animation text-sm-5 textSize text-style'
-                data-appear-animation='fadeInUpShorter'
-                data-appear-animation-delay='300'
-              >
-                Phone : <strong className='text-dark'>+1 714-455-2897</strong>
-              </p>
+          <section
+            className="d-flex justify-content-between p-4"
+            style={{ backgroundColor: "#6351ce" }}
+          >
+            <div className="me-5">
+              <span>Get connected with us on social networks:</span>
             </div>
-            <div className='col-6 col-lg-2 mb-5 mb-lg-0 text-sm-4 d-flex flex-column justify-content-center align-start'>
-              <ul className='list-style'>
-                <li className='li-elem'>
-                <i className='fa fa-angle-right'></i>
-                  <a
-                    href='https://www.facebook.com/tvdhwani'
-                    className='link-hover-style-1 text-white ms-1'
-                    target='_blank'
-                  >
-                    {' '}
-                    Facebook
-                  </a>
-                </li>
-                <li className='li-elem'>
-                <i className='fa fa-angle-right'></i>
-                  <a
-                    href='https://www.instagram.com/tvdhwani'
-                    className='link-hover-style-1 text-white ms-1'
-                    target='_blank'
-                  >
-                    {' '}
-                    Instagram
-                  </a>
-                </li>
-                <li className='li-elem'>
-                <i className='fa fa-angle-right'></i>
-                  <a
-                    href='https://www.instagram.com/dhwaniacademy/?hl=en'
-                    className='link-hover-style-1 text-white ms-1'
-                  >
-                    {' '}
-                    Youtube
-                  </a>
-                </li>
-                <li className='li-elem'>
-                <i className='fa fa-angle-right'></i>
-                  <a
-                    href='https://tvdhwani.com/home'
-                    className='link-hover-style-1 text-white ms-1'
-                  >
-                    {' '}
-                    Google
-                  </a>
-                </li>
-              </ul>
+            <div>
+              <a href="" className="text-white me-4">
+                <i className="fab fa-facebook-f"></i>
+              </a>
+              <a href="" className="text-white me-4">
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a href="" className="text-white me-4">
+                <i className="fab fa-google"></i>
+              </a>
+              <a href="" className="text-white me-4">
+                <i className="fab fa-instagram"></i>
+              </a>
+              <a href="" className="text-white me-4">
+                <i className="fab fa-linkedin"></i>
+              </a>
+              <a href="" className="text-white me-4">
+                <i className="fab fa-github"></i>
+              </a>
             </div>
-          </div>
-          <div className='text-center p-3 footer-bg'>
-            <label>© 2024 Copyright&nbsp;:&nbsp;</label>
-            <a
-              className='text-white footer-copyright-content'
-              href='https://www.dhwaniacademy.in/'
-            >
-              TV Dhwani
+          </section>
+
+          <section className="">
+            <div className="container text-start text-md-start mt-5">
+              <div className="row mt-3">
+                <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                  <h6 className="text-uppercase fw-bold">Company name</h6>
+                  <hr
+                    className="mb-4 mt-0 d-inline-block mx-auto"
+                    style={{
+                      width: "60px",
+                      backgroundColor: "#7c4dff",
+                      height: "2px",
+                    }}
+                  />
+
+                  <p>
+                    Here you can use rows and columns to organize your footer
+                    content. Lorem ipsum dolor sit amet, consectetur adipisicing
+                    elit.
+                  </p>
+                </div>
+
+                <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                  <h6 className="text-uppercase fw-bold">Products</h6>
+                  <hr
+                    className="mb-4 mt-0 d-inline-block mx-auto"
+                    style={{
+                      width: "60px",
+                      backgroundColor: "#7c4dff",
+                      height: "2px",
+                    }}
+                  />
+
+                  <p>
+                    <a href="#!" className="text-white">
+                      MDBootstrap
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-white">
+                      MDWordPress
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-white">
+                      BrandFlow
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-white">
+                      Bootstrap Angular
+                    </a>
+                  </p>
+                </div>
+                <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                  <h6 className="text-uppercase fw-bold">Useful links</h6>
+                  <hr
+                    className="mb-4 mt-0 d-inline-block mx-auto"
+                    style={{
+                      width: "60px",
+                      backgroundColor: "#7c4dff",
+                      height: "2px",
+                    }}
+                  />
+
+                  <p>
+                    <a href="#!" className="text-white">
+                      Your Account
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-white">
+                      Become an Affiliate
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-white">
+                      Shipping Rates
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-white">
+                      Help
+                    </a>
+                  </p>
+                </div>
+                <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                  <h6 className="text-uppercase fw-bold">Contact</h6>
+                  <hr
+                    className="mb-4 mt-0 d-inline-block mx-auto"
+                    style={{
+                      width: "60px",
+                      backgroundColor: "#7c4dff",
+                      height: "2px",
+                    }}
+                  />
+
+                  <p>
+                    <i className="fas fa-home mr-3"></i> New York, NY 10012, US
+                  </p>
+                  <p>
+                    <i className="fas fa-envelope mr-3"></i> info@example.com
+                  </p>
+                  <p>
+                    <i className="fas fa-phone mr-3"></i> + 01 234 567 88
+                  </p>
+                  <p>
+                    <i className="fas fa-print mr-3"></i> + 01 234 567 89
+                  </p>
+                </div>
+              </div>
+            </div>
+          </section>
+          <div
+            className="text-center p-3"
+            style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+          >
+            © 2020 Copyright:
+            <a className="text-white" href="https://mdbootstrap.com/">
+              MDBootstrap.com
             </a>
           </div>
         </footer>
-      </>
-    )
-  }
-}
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
